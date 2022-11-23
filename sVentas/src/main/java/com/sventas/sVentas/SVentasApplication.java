3bcd51a12d0841950c6105f1f62cfc8ec6dfb545
Richard Nagy <102683011+richardnaggy@users.noreply.github.com>
2022-11-23 13:18:28 -0300

    Add files via upload

---
+++ sVentas/src/main/java/com/sventas/sVentas/SVentasApplication.java
@@ -0,0 +1,13 @@
+package com.sventas.sVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SVentasApplication.class, args);
+	}
+
+}
---
+++ sVentas/src/test/java/com/sventas/sVentas/SVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sventas.sVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
